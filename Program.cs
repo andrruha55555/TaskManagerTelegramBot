44f047c64778d3c818df8c4b703e359b81883d18
AluonaUzhakova <alyonauzhakova@gmail.com>
2025-12-17 12:30:00 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+using TaskManagerTelegramBot_Pikulev;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
