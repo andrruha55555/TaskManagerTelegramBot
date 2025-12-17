4eb82a57447aee50cf2b075dc0c3a87ad29648e9
AluonaUzhakova <alyonauzhakova@gmail.com>
2025-12-17 12:37:29 +0500

    Созданный классы для приложения. Пикулев

---
+++ Classes/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagerTelegramBot_Pikulev.Classes
+{
+    public class User
+    {
+        public long IdUser { get; set; }
+        public List<Events> Events { get; set; }
+        public User(long idUser)
+        {
+            IdUser = idUser;
+            Events = new List<Events>();
+        }
+    }
+}
